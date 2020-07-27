1259872e4df68c197c5aa05cd472a6d85c79da2f
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2020-07-27 13:44:36 +0530

    singly liked final

--- Linkedlist/linked.java
+++ Linkedlist/linked.java
@@ -1,5 +1,5 @@
 public class linked{
-
+    //basic node creation
     class Node { 
         int val; 
         Node next; 
@@ -18,11 +18,52 @@ public class linked{
         l1.insert(42);
         l1.insert(423);
         l1.insert(4206);
-        System.out.println("Original List:");
+        System.out.println("Before:");
         l1.display();
         l1.insertafter(9, 4206);
         l1.insertbefore(0, 423);
+        l1.insertbeg(53);
+        l1.delend();        
+        l1.delbegin();
+        l1.insertend(987);
         l1.display();
+    }
+    //begning
+    public void insertbeg(int data) {
+        Node ptr = head;
+        Node newNode = new Node(data);
+        newNode.next = ptr;
+        head = newNode;
+        System.out.println("Added " + data + " at beginning of list.");
+    }
+
+    public void insertend(int data) {
+        Node ptr = tail;
+        Node newNode = new Node(data);
+        ptr.next = newNode;
+        tail = newNode;
+        System.out.println("Added " + data + " at end of list.");
+    }
+    
+    public void delbegin(){
+        if(head == null) {  
+            System.out.println("Underflow");
+            return;  
+        }  
+        Node ptr = head;
+        head = ptr.next;
+        System.out.println("Deleted Node from beginning of list.");
+    }
+
+    public void delend() {
+        Node ptr = head;
+        Node preptr = ptr;
+        while (ptr.next!=null){
+            preptr = ptr;
+            ptr = ptr.next;
+        }
+        preptr.next = null;
+        System.out.println("Deleted Node from the end of the list.");
     }
 
 
